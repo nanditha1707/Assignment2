a65a28326113b344a500dbc410e14070a9db5b42
nanditha1707 <s10258670@connect.np.edu.sg>
2024-02-03 03:01:39 +0800

    footer update and brochure

--- script.js
+++ script.js
@@ -95,3 +95,16 @@ const questions = [
   }
 
   loadQuestion();
+
+
+  // footer form
+  function openModal() {
+    document.getElementById('modal').style.display = 'block';
+}
+function closeModal() {
+   document.getElementById('modal').style.display = 'none';
+}
+function submitForm() {
+   closeModal();
+}
+
